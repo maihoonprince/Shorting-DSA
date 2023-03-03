d1e97b4e5bc2e99129d76e95956980b4d2cc9e3a
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-03-03 22:31:54 +0530

    Add files via upload

---
+++ bubbleShorting.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class BubbleShorting 
+{
+    public static void printArray(int arr[])
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        int arr[] = {7,8,3,1,2}; 
+
+        //Time Complexity = 0(n^2)
+        //bubble sort
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j < arr.length-i-1; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
