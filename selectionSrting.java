ccc649c61e037d249f4a1e89814e99bf50601fbf
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-03-06 11:45:44 +0530

    Add files via upload

---
+++ selectionSrting.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class selectionSrting 
+{
+    public static void printArray(int arr[])
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        int arr[] = {7,8,3,1,2}; 
+
+        //Time Complexity = 0(n^2)
+        //selectin sort
+        for(int i=0; i<arr.length-1; i++)
+        {
+            int smallest = i;
+            for(int j =i+1; j<arr.length; j++)
+            {
+                if(arr[smallest] > arr[j])
+                {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
